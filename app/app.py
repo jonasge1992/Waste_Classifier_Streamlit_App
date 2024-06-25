bee5e5a2ca84619f9e09e1e509d9024fac86c884
jonas gebhardt <jonasgebhardt@gmx.net>
2024-06-25 10:33:31 -0500

    added translations for class name

--- app/app.py
+++ app/app.py
@@ -5,6 +5,40 @@ from PIL import Image
 import io
 import os
 
+class_labels_translation = {
+    'aerosol_cans': 'Aerosol Cans',
+    'aluminum_food_cans': 'Aluminum Food Cans',
+    'aluminum_soda_cans': 'Aluminum Soda Cans',
+    'cardboard_boxes': 'Cardboard Boxes',
+    'cardboard_packaging': 'Cardboard Packaging',
+    'clothing': 'Clothing',
+    'coffee_grounds': 'Coffee Grounds',
+    'disposable_plastic_cutlery': 'Disposable Plastic Cutlery',
+    'eggshells': 'Eggshells',
+    'food_waste': 'Food Waste',
+    'glass_beverage_bottles': 'Glass Beverage Bottles',
+    'glass_cosmetic_containers': 'Glass Cosmetic Containers',
+    'glass_food_jars': 'Glass Food Jars',
+    'magazines': 'Magazines',
+    'newspaper': 'Newspaper',
+    'office_paper': 'Office Paper',
+    'paper_cups': 'Paper Cups',
+    'plastic_cup_lids': 'Plastic Cup Lids',
+    'plastic_detergent_bottles': 'Plastic Detergent Bottles',
+    'plastic_food_containers': 'Plastic Food Containers',
+    'plastic_shopping_bags': 'Plastic Shopping Bags',
+    'plastic_soda_bottles': 'Plastic Soda Bottles',
+    'plastic_straws': 'Plastic Straws',
+    'plastic_trash_bags': 'Plastic Trash Bags',
+    'plastic_water_bottles': 'Plastic Water Bottles',
+    'shoes': 'Shoes',
+    'steel_food_cans': 'Steel Food Cans',
+    'styrofoam_cups': 'Styrofoam Cups',
+    'styrofoam_food_containers': 'Styrofoam Food Containers',
+    'tea_bags': 'Tea Bags'
+}
+
+
 def send_image_to_api(image):
     # Convert the image to bytes
     img_bytes = io.BytesIO()
@@ -37,8 +71,10 @@ if picture is not None:
     # Send the image to the API and get the prediction
     result = send_image_to_api(image)
 
+    proper_name = class_labels_translation[result["class_name"]]
+
     # Display the prediction result
-    st.write("Prediction:", result["class_name"])
+    st.write("Prediction:", proper_name)
 
 # Custom CSS for styling
 st.markdown(
